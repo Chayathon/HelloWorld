d09d264c43b33cb43751148706903795dbb79b0c
wasuchok jainam <wasuchokop@gmail.com>
2023-07-17 19:04:47 +0700

    lastupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
+
+  const increment_quantity = () => {
+    setCount(count + 1)
+  }
+
+  const decrement_quantity = () => {
+    setCount(count - 1)
+  }
+
+  useEffect(() => {
+    if(count == 0) {
+      alert("มึงกดน้อยกว่า 0 แล้วนะ")
+      setCount(1)
+    }
+  },[count])
 
   return (
     <>
@@ -16,14 +33,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>wasd</h1>
+      <h1>ทดสอบการคลิก</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>{count}</h1>
+        <button onClick={increment_quantity}>เพิ่ม</button>
+        <button onClick={decrement_quantity}>ลด</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
